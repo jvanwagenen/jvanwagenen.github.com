bfd4d5a3db3fada044d4a1fd453ac8f0c328944c
pablobarria <pablo.barria@post.harvard.edu>
2016-09-24 18:30:55 -0400

    shifting texxt!

---
+++ js/scripts.js
@@ -0,0 +1,73 @@
+function changeWords(){
+  var firstWords = [
+    ['modern'],
+    ['mytho','logy'],
+    ['mitol','ogia'],
+    ['modern','a']
+  ];
+
+  var moreWords = [
+    ['material', 'ity', 'ità'],
+    ['possibil', 'ity', 'ità', 'idad'],
+    ['celebr', 'ity', 'ità'],
+    ['sublim', 'ity', 'ità', 'idad'],
+    ['normal', 'ity', 'ità'],
+    ['conform', 'ity', 'ità'],
+    ['visibil', 'ity', 'ità'],
+    ['plural', 'ity', 'ità', 'idad'],
+    ['individual', 'ity', 'ità'],
+    ['curios', 'ity', 'ità', 'idad'],
+    ['modern', 'ity', 'ità'],
+    ['fractal', 'idad', 'ity'],
+    ['infin', 'ity', 'ità', 'idad'],
+    ['real','ism', 'ismo'],
+    ['modern','ism', 'ismo'],
+    ['postmodern','ism', 'ismo'],
+    ['capital','ism', 'ismo'],
+    ['social','ism', 'ismo'],
+    ['anarch','ism', 'ismo']
+  ];
+
+  var clock = document.getElementById('clock');
+  var left = clock.querySelector('#left');
+  var right = clock.querySelector('#right');
+
+  var flatPairs = moreWords.sort(randomSort).map(function(wordGroup){
+    var stem = wordGroup.shift();
+    var pairs = wordGroup.map(function(ending){
+      return [stem, ending];
+    });
+    return pairs;
+  }).reduce(function(previous,current){
+    current.forEach(function(pair){
+      previous.push(pair);
+    });
+    return previous;
+  },[]);
+
+  var allPairs = firstWords.concat(flatPairs);
+  var counter = 0;
+
+  function setWord(words){
+    if(words[0]){
+      left.textContent = words[0];
+    } else {
+      left.textContent = '';
+    }
+    if(words[1]){
+      right.textContent = words[1];
+    } else {
+      right.textContent = '';
+    }
+  }
+
+  function randomSort(){
+    return Math.random() - 0.5;
+  }
+
+  return function(){
+    setWord(allPairs[counter]);
+    counter = (counter + 1) % allPairs.length;
+  }
+
+}
